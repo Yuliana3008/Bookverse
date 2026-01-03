7d6a7206cc248c4708238b5f6a94639665f6c7ee
Yuliana Sanchez <yulianaurbinasanchez@gmail.com>
2026-01-03 12:45:39 -0600

    fix vite config for vercel

--- bookverse/vite.config.js
+++ bookverse/vite.config.js
@@ -1,18 +1,11 @@
-/*import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-export const API_URL = "http://localhost:4000";
-
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})*/
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import * as path from 'path'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
