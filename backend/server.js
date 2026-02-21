e105e9be685c6b7baef05959bf6d84fe29506ca3
Yuliana Sanchez <yulianaurbinasanchez@gmail.com>
2026-02-21 01:08:19 -0600

    Cambio dominio

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,8 @@ app.set("trust proxy", 1);
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173", 
-  "https://bookverse-git-main-yuliana-sanchezs-projects.vercel.app",
+  "https://mybookcompass.xyz",
+  "https://www.mybookcompass.xyz",
   ...(process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : []),
 ];
 
